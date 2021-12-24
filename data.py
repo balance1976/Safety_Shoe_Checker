f967885752ebb17b69431c9997163213b1475d89
Ryan Ramirez <ryaneramirez@gmail.com>
2021-12-23 22:31:55 -0800

    The interface look correct and the data is being stored in a text file.

--- data.py
+++ data.py
@@ -1,6 +1,7 @@
 import os
 
 np = os.path.dirname(__file__)
+
 def writescan(code):
     with open(np + "\\scannedbadges.txt",'a') as of:
         of.write("\n")
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, url_for, request, redirect
-
+from data import *
 app = Flask('__name__')
 
 @app.route('/', methods=["GET","POST"])
 def home():
     if request.method == 'POST':
-        print(request.method)
-        return "post returned"
+        rf = request.form['barcode']
+        print("postie", rf)
+        writescan(rf)
+        print(request.form)
+        return render_template('base.html')
     else:
         return render_template('base.html')
 
